2c85d563fefd93a3fbbbca2c70f79b8a013e3aa9
lakshmi2244 <golilakshmiprasanna2244@gmail.com>
2022-11-23 17:11:33 +0530

    express

---
+++ data.js
@@ -0,0 +1,7 @@
+const students=[
+   {id:1,stdname:"hardin",stdclass:"cse"},
+   {id:2,stdname:"tessa",stdclass:"ece"},
+   {id:3,stdname:"marcus",stdclass:"eee"}
+]
+
+module.exports=students
---
+++ index.js
@@ -0,0 +1,20 @@
+const express=require("express")
+const app=express()
+const cors=require("cors")
+const port=2000;
+const employee={id:26,name:"node",position:"frontend"}
+const students=require("./data.js")
+app.use(cors())
+app.get("/",(req,res)=>{
+	res.send("starting route")
+})
+app.get("/demo",(req,res)=>{
+	res.send("this is demo route")
+})
+app.get("/emp",(req,res)=>{
+	res.json(employee)
+})
+app.get("/students-list",(req,res)=>{
+	res.json(students)
+})
+app.listen(port,()=>console.log("server is running on port 2000"))
